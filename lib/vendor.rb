19d1a4717d422caf49918ee6210f634f9c291e30
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-26 10:11:29 -0700

    update vendor_inventory to correct data type

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -2,6 +2,6 @@ class Vendor
   attr_reader :name, :vendor_inventory
   def initialize(name)
     @name = name
-    @vendor_inventory = []
+    @vendor_inventory = {}
   end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -15,6 +15,31 @@ class VendorTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal "Rocky Mountain Fresh", @vendor.name
-    assert_equal [], @vendor.vendor_inventory
+    assert_equal {}, @vendor.vendor_inventory
   end
+
+
+#   pry(main)> vendor.inventory
+# #=> {}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+#
+# pry(main)> vendor.stock(item1, 30)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+#
+# pry(main)> vendor.stock(item1, 25)
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+#
+# pry(main)> vendor.stock(item2, 12)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<
 end
