35a6f1b22f6c5a789aeb0436629f1a3110c0bdf6
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-26 10:08:32 -0700

    Write and pass test_it_exists

---
+++ lib/vendor.rb
@@ -0,0 +1,5 @@
+class Vendor
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ test/vendor_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+
+class VendorTest < Minitest::Test
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+end
