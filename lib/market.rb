f9d9edb94945cdcc5e01cce2fb6485cbf2d50671
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-26 11:14:00 -0700

    Pass test_it_can_add_vendor

--- lib/market.rb
+++ lib/market.rb
@@ -4,4 +4,8 @@ class Market
     @name = name
     @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -9,6 +9,11 @@ require './lib/market'
 class MarketTest < Minitest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exists
@@ -18,5 +23,14 @@ class MarketTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_vendors
+    @market.add_vendor(@vendor1)
+    assert_equal [@vendor1], @market.vendors
+
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 end
