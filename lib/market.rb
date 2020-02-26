a06d9b24844b9a79d34ca592651cfd836312601b
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-26 11:10:51 -0700

    Pass test_it_has_attributes

--- lib/market.rb
+++ lib/market.rb
@@ -1,5 +1,7 @@
 class Market
+  attr_reader :name, :vendors
   def initialize(name)
     @name = name
+    @vendors = []
   end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -14,4 +14,9 @@ class MarketTest < Minitest::Test
   def test_it_exists
     assert_instance_of Market, @market
   end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
 end
